04fc9a7db278dd8a69714149b8eebd403a4e73f7
VogelDev <rvogel@mc3.edu>
2017-08-05 21:56:49 -0400

    npm/grunt/webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+var path = require('path')
+var webpack = require('webpack')
+
+function resolve (dir) {
+  return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, 'site'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]?[hash]'
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.vue', '.json'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js',
+            '@': resolve('src')
+        }
+    },
+    performance: {
+        hints: false
+    },
+    devtool: '#eval-source-map',
+    // node: {
+    //     fs: 'empty'
+    // }
+}
+
+// if (process.env.NODE_ENV === 'production') {
+//     module.exports.devtool = '#source-map'
+//     // http://vue-loader.vuejs.org/en/workflow/production.html
+//     module.exports.plugins = (module.exports.plugins || []).concat([
+//         new webpack.DefinePlugin({
+//             'process.env': {
+//                 NODE_ENV: '"production"'
+//             }
+//         }),
+//         new webpack.optimize.UglifyJsPlugin({
+//             sourceMap: true,
+//             compress: {
+//                 warnings: false
+//             }
+//         }),
+//         new webpack.LoaderOptionsPlugin({
+//             minimize: true
+//         })
+//     ])
+// }
